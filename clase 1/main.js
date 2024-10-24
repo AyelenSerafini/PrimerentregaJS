997ecc553b2f72c872228269644ca4342ad27fdf
¨Ayelen <ayelenserafini1996@gmail.com>
2024-10-23 21:35:04 -0300

    subiendo proyecto

---
+++ clase 1/main.js
@@ -0,0 +1,36 @@
+// Variables Constantes//
+
+let nombre 
+let eded
+let apellido
+const url = "www.google.com"
+
+apellido = "Samsung"
+nombre = "Samsung"
+eded = 28
+
+
+console.log(nombre)
+console.log(eded)
+console.log(url)
+console.log(nombre + " " + apellido)
+
+// Alert Confirm Prompt //
+
+alert("Bienvenidos")
+
+confirm("¿Eres mayor de edad?")
+
+prompt("¿Cuantos años tienes?")
+
+
+//Array//
+
+const celulares = ["Samsung", "Motorola", "Lg", "Nokia" ]
+const productos = ["Auriculares", "cargadores", "Fundas","Protectores"]
+
+const Arraynumero1 = [1,2,3,4]
+
+console.log(Arraynumero1)
+console.log(productos)
+console.log(celulares)
